10f2d6de71561541a20870be0e45fa25803b2e5b
philoem <philoem@nexgate.ch>
2018-05-13 20:56:48 +0200

    Test security login

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,20 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 use App\Entity\Members;
 use App\Form\LoginType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends Controller
 {
     /**
      * @Route("/login", name="login")
      */
-    public function login(Request $request)
+    public function login(Request $request, AuthenticationUtils $authenticationUtils)
     {
+        
         $member = new Members();
 
         $form = $this->createForm(LoginType::class, $member);
