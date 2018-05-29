a6812fb43e22082d84defc5e4c01fe2f646542f0
philoem <philoem@nexgate.ch>
2018-05-29 14:38:23 +0200

    Mise en place de SwiftMailer

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,6 +6,7 @@ use App\Entity\Contact;
 use App\Form\ContactType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -31,8 +32,12 @@ class ContactController extends Controller
             $em->persist($contact);
             $em->flush();
             
+            $this->addFlash(
+                'notice',
+                'Votre message a bien été envoyé !'
+            );
                        
-            
+            return $this->redirectToRoute('contact');
         
         }
 
